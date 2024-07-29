b1d466862014d04a349e24baa8fa6610081e9e6e
yutaka3koyama <159596003+yutaka3koyama@users.noreply.github.com>
2024-07-29 13:15:37 +0900

    Update two_layer_net.py
    delete sys.os

--- 天気予報/two_layer_net.py
+++ 天気予報/two_layer_net.py
@@ -1,6 +1,4 @@
 # coding: utf-8
-import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 from common.functions import *
 from common.gradient import numerical_gradient
 import numpy as np
